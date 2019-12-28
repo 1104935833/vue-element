9673b06c5108f3998189158259bfb84ec20f8173
1104935833 <1104935833@qq.com>
2019-12-28 14:03:01 +0800

    ini

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,22 @@ Vue.use(Router)
 
 const router = new Router({
     routes: [{
-        path: '/',
-        name: 'Login',
-        component: Login,
-        hidden: true
-    }, {
-        path: '/home',
-        name: '主页',
-        component: Home,
-        hidden: true,
-        meta: {
-            requireAuth: true
-        },
-    }]
+            path: '/',
+            name: 'Login',
+            component: Login,
+            hidden: true
+        }, {
+            path: '/home',
+            name: '主页',
+            component: Home,
+            hidden: true,
+            meta: {
+                requireAuth: true
+            },
+        }
+
+
+    ]
 })
 router.beforeEach((to, from, next) => {
     if (to.path === '/login') {
