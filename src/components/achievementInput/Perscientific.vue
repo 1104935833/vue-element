6c905f721c1e08fe363498c738fd2105a22ecaf9
Wuyinsong <1595759921@qq.com>
2020-02-08 16:48:30 +0800

    荣誉录入系统界面修改

---
+++ src/components/achievementInput/Perscientific.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 align="center">个人科研业绩录入</h1>
+    <h1 align="center">集体科研业绩录入</h1>
+  </div>
+</template>
---
+++ src/components/achievementInput/Perteacher.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 align="center">个人科研业绩</h1>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <h1 align="center">个人教学业绩</h1>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>   
+  </div>
+</template>
