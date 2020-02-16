a8aae7c5e3b2fa8f158f4a7bd26f796c4460552d
1104935833 <1104935833@qq.com>
2020-02-16 16:02:55 +0800

    Merge branch 'master' of github.com:1104935833/vue-elementgit reset --merge HEAD

--- src/components/system/centerChart/Chart1.vue
+++ src/components/system/centerChart/Chart1.vue
@@ -10,50 +10,51 @@ import echarts from "echarts";
 //   import "echarts/lib/chart/bar";
 export default {
   data() {
-    return {};
+    return {
+      data: []
+    };
   },
   mounted() {
-    let myChart = this.echarts.init(document.getElementById("chart_example"));
-    let option = {
-      title: {
-        text: "所有业绩获得情况",
-        left: "center"
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-      legend: {
-        orient: "vertical",
-        left: "left",
-         data: ["科研", "荣誉", "教学"]
-      },
-      series: [
-        {
-          name: "业绩情况",
-          type: "pie",
-          radius: "55%",
-          center: ["50%", "60%"],
-          data: [
-            { value: 335, name: "科研" },
-            { value: 310, name: "荣誉" },
-            { value: 234, name: "教学" }
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: "rgba(0, 0, 0, 0.5)"
+    this.getRequest("/center/PerforEchart", { year: 3 }).then(res => {
+      let option = {
+        title: {
+          text: "所有业绩获得情况",
+          left: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: ["科研", "荣誉", "教学"]
+        },
+        series: [
+          {
+            name: "业绩情况",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: res.data,
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
             }
           }
-        }
-      ]
-    };
-    myChart.setOption(option);
+        ]
+      };
+      let myChart = this.echarts.init(document.getElementById("chart_example"));
 
-    //建议加上以下这一行代码，不加的效果图如下（当浏览器窗口缩小的时候）。超过了div的界限（红色边框）
-    window.addEventListener("resize", function() {
-      myChart.resize();
+      myChart.setOption(option);
+
+      //建议加上以下这一行代码，不加的效果图如下（当浏览器窗口缩小的时候）。超过了div的界限（红色边框）
+      window.addEventListener("resize", function() {
+        myChart.resize();
+      });
     });
   },
   methods: {},
