edcd6c66fbb4cf1edcc6234c94ccb415fcdf405d
Wuyinsong <1595759921@qq.com>
2020-02-20 15:37:31 +0800

    20200220_2

---
+++ src/components/achievementInput/Perteacher.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    
+    
+  </div>  
+</template>
+<script>
+import personal from "./Teacher/personalteacher";
+export default {
+  components:{
+    Personal:personal,
+  }
+}
+</script>
---
+++ src/components/achievementInput/Teacher/personalteacher.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <xuekejingsaizhidao></xuekejingsaizhidao>
+    </div>
+</template>
+
+<script>
+import xuekejingsaizhidao from "./P-xuekejingsaizhidao";
+export default {
+  components: {
+    Xuekejingsaizhidao:xuekejingsaizhidao,
+  },
+}
+</script>
